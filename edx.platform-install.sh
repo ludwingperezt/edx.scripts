f119744ff0fbc2b5b2a4fde1fb625662a8838341
Ludwing Pérez <ludwing@ludwing-idei>
2019-01-31 12:21:40 -0600

    Modificado el script de instalación para configurar locale para español-guatemala

--- edx.platform-install.sh
+++ edx.platform-install.sh
@@ -3,7 +3,7 @@
 # McDaniel
 # October 2017
 #
-# To stand up a pristine single-server instance of Open edX Ginkgo.1
+# To stand up a pristine single-server instance of Open edX hawthorn.master
 # this is a modification of the generic instructions from:
 #   Native Open edX Ubuntu 16.04 64 bit Installation
 #   https://openedx.atlassian.net/wiki/spaces/OpenOPS/pages/146440579/Native+Open+edX+Ubuntu+16.04+64+bit+Installation
@@ -16,38 +16,18 @@
 cd ~
 
 # Prerequisites: ensure that locales are set on your server. if not the ansible boostrap script below will break.
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-
-# Delve into these settings only if you are installing a lanuage other than English
-#sudo locale-gen es_ES es_ES.UTF-8
-#sudo dpkg-reconfigure locales#sudo dpkg-reconfigure locales
-
-#export LANG=es_ES.UTF-8
-#export LC_ALL="es_ES.UTF-8"
-#export LC_CTYPE="es_ES.UTF-8"
-
-#export LC_CTYPE=?~@~]es_ES.UTF-8?~@~]
-#export LC_NUMERIC=?~@~]es_ES.UTF-8?~@~]
-#export LC_TIME=?~@~]es_ES.UTF-8?~@~]
-#export LC_COLLATE=?~@~]es_ES.UTF-8?~@~]
-#export LC_MONETARY=?~@~]es_ES.UTF-8?~@~]
-#export LC_MESSAGES=?~@~]es_ES.UTF-8?~@~]
-#export LC_PAPER=?~@~]es_ES.UTF-8?~@~]
-#export LC_NAME=?~@~]es_ES.UTF-8?~@~]
-#export LC_ADDRESS=?~@~]es_ES.UTF-8?~@~]
-#export LC_TELEPHONE=?~@~]es_ES.UTF-8?~@~]
-#export LC_MEASUREMENT=?~@~]es_ES.UTF-8?~@~]
-#export LC_IDENTIFICATION=?~@~]es_ES.UTF-8?~@~]
-
+# Setting locale to spanish-guatemala
+sudo locale-gen es_GT es_GT.UTF-8
+sudo localectl set-locale LANG=es_GT.UTF-8
+export LANG=es_GT.UTF-8
+export LC_ALL="es_GT.UTF-8"
+export LC_CTYPE="es_GT.UTF-8"
 
 # 1. Set the OPENEDX_RELEASE variable:
 #export OPENEDX_RELEASE=open-release/ginkgo.2
 # Note: sometimes there are important bug fixes in master that are not included in the named releases.
 #       to date i've always had the best luck with master.
 export OPENEDX_RELEASE=open-release/hawthorn.master
-
-
 
 # 2. Bootstrap the Ansible installation:
 wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -H bash
